17a53e423660fae4882f55c4efa5c656b104a0f0
jpluomajoki <jpluomajoki@gmail.com>
2020-07-06 13:48:18 +0300

    chore: Clean up clutter from project generator

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,4 @@
-import { Router } from 'express';
-import UserRouter from './Users';
+import {Router} from 'express';
 import BeerRouter from './Beer';
 
 // Init router and path
@@ -7,7 +6,6 @@ const router = Router();
 console.log(BeerRouter);
 
 // Add sub-routes
-//router.use('/users', UserRouter);
 router.use('/beer', BeerRouter);
 
 // Export the base-router
