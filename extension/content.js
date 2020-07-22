c21293c522cc6ddfb0f035e5f7fda29b70294661
jpluomajoki <jpluomajoki@gmail.com>
2020-07-22 12:56:02 +0300

    inject beer icon to beers found in alko.fi

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,22 @@
 console.log("content script loaded");
+const products = Array.from(
+  document.getElementsByClassName("product-data-container")
+);
+console.log(products);
+
+const beerElements = products.filter(
+  (product) =>
+    JSON.parse(product.getAttribute("data-product-data")).category === "Oluet"
+);
+console.log(beerElements);
+console.log(`found ${beerElements.length} beers`);
+
+beerElements.forEach((elem) => {
+  const scoreDiv = document.createElement("div");
+  const imgEl = document.createElement("img");
+  // TODO: Fix icon styling
+  imgEl.src = chrome.extension.getURL("icons/beer_32.png");
+  // TODO: Add onclick functionality to search for a beer
+  scoreDiv.appendChild(imgEl);
+  elem.appendChild(scoreDiv);
+});
