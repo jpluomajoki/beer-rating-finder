9dc9908d50efec7b33c8f7ed6fa9c705dca3d002
jpluomajoki <jpluomajoki@gmail.com>
2020-08-08 22:28:42 +0300

    Fix beer icon positioning

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,3 @@
-console.log("content script loaded");
 const products = Array.from(
   document.getElementsByClassName("product-data-container")
 );
@@ -9,12 +8,21 @@ const beerElements = products.filter(
     JSON.parse(product.getAttribute("data-product-data")).category === "Oluet"
 );
 console.log(beerElements);
-console.log(`found ${beerElements.length} beers`);
 
 beerElements.forEach((elem) => {
   const scoreDiv = document.createElement("div");
+  scoreDiv.classList.add("beer-rating");
+  const addToCartVisible = !Array.from(elem.children)
+    .find((e) => e.className == "mc-add-to-cart-button")
+    .children[0].classList.contains("is-disabled");
+  scoreDiv.setAttribute(
+    "style",
+    `z-index: 2; cursor: pointer; position: absolute; top: 33px; left: ${
+      addToCartVisible ? "84px" : "46px"
+    }`
+  );
   const imgEl = document.createElement("img");
-  // TODO: Fix icon styling
+  // TODO: Get better icon
   imgEl.src = chrome.extension.getURL("icons/beer_32.png");
   // TODO: Add onclick functionality to search for a beer
   scoreDiv.appendChild(imgEl);
